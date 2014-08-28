234dd4077c2636d68738590cdd6b86940d98bcfb
sph <tantalum@online.de>
2014-08-28 22:03:55 +0200

    -

--- sph/lang/c/expressions.scm
+++ sph/lang/c/expressions.scm
@@ -1,66 +1,67 @@
 (library (sph lang c expressions)
   (export
-    cp-if
-    cp-undef
-    cp-define-macro
-    cp-define-macro-nc
-    cp-include
-    c-statement-nc
-    cp-concat-nc
-    cp-includep
-    c-parameter
+    c-address-of
     c-apply
     c-apply-nc
+    c-bit-shift-nc
+    c-compound-nc
+    c-convert-type-nc
     c-define
     c-define-nc
     c-function
-    c-function-pointer-nc
     c-function-nc
+    c-function-pointer-nc
     c-identifier
+    c-if
     c-if-statement
-    c-compound-nc
-    c-struct-ref
-    c-convert-type-nc
-    c-typedef-nc
-    c-typedef-function-nc
-    c-stringify
-    c-struct-ref-nc
-    c-pointer-deref-nc
-    c-address-of
-    c-pointer
-    c-bit-shift-nc
     c-line-nc
+    c-parameter
+    c-pointer
+    c-pointer-deref-nc
     c-set
     c-set-nc
+    c-statement-nc
     c-string
-    c-if
+    c-stringify
+    c-struct-ref
+    c-struct-ref-nc
+    c-typedef-function-nc
+    c-typedef-nc
     c-value
     c-vector
     c-vector-nc
+    cp-concat-nc
+    cp-define-macro
+    cp-define-macro-nc
+    cp-if
+    cp-include
+    cp-includep
+    cp-undef
     list->c-vector)
   (import
-    (rnrs base)
-    (only (rnrs hashtables) hashtable?)
     (guile)
+    (rnrs base)
+    (sph)
+    (sph alist)
     (sph hashtable)
     (sph list)
-    (sph alist)
     (sph string)
-    (sph))
+    (only (rnrs hashtables) hashtable?))
 
   ;generating c expressions as strings.
+
   ;by default, the functions convert input values from scheme datatypes,
+
   ;the variants with a -nc suffix are non-converting and take strings.
 
   (define (c-stringify arg) (string-append "#" arg))
   (define (cp-undef arg) (string-append "#undef " arg))
   (define (cp-include path) (string-append "#include " (c-string path)))
-  (define (cp-includep path) (string-append "#include <"  path ">"))
+  (define (cp-includep path) (string-append "#include <" path ">"))
   (define (cp-concat-nc args) (string-join args "##"))
 
   (define (cp-define-macro-nc name body identifier-list)
-    (string-append
-      "#define " name (if identifier-list identifier-list "") " " body))
+    (string-append "#define " name (if identifier-list identifier-list "") " " body))
 
   (define* (cp-define-macro name body #:optional identifier-list)
     (cp-define-macro-nc (c-identifier name) body (map c-identifier identifier-list)))
@@ -69,13 +70,9 @@
     (string-append "#"
       (if (eq? (q if) type) "if"
         (if (eq? (q ifdef) type) "ifdef"
-          (if (eq? (q ifndef) type) "ifndef"
-            (raise (q cannot-convert-to-c)))))
-      " " test "\n" consequent "\n"
-      (if alternate
-        (string-append "#else\n" alternate "\n")
-        "")
-      "#endif"))
+          (if (eq? (q ifndef) type) "ifndef" (raise (q cannot-convert-to-c)))))
+      " " test
+      "\n" consequent "\n" (if alternate (string-append "#else\n" alternate "\n") "") "#endif"))
 
   (define (c-compound-nc arg) (string-append "{" arg "}"))
   (define (c-typedef-nc name arg) (c-line-nc "typedef" arg name))
@@ -86,20 +83,15 @@
   (define (c-convert-type-nc arg type) (string-append "(" type ")(" arg ")"))
   (define (c-line-nc . arg) (string-join arg " "))
 
-  (define* (c-statement-nc keyword body #:optional arg)
-    (string-append keyword
-      (if arg (parenthesise arg) "")
-      (c-compound-nc body)))
+  (define* (c-statement-nc keyword body #:optional prefix-arg suffix-arg)
+    (string-append keyword (if prefix-arg (parenthesise prefix-arg) "")
+      (c-compound-nc body) (if suffix-arg (parenthesise suffix-arg) "")))
 
   (define (c-enum-nc name enum-list)
     (string-append "enum " name
       (c-compound-nc
         (string-join
-          (map
-            (l (ele)
-              (if (list? ele)
-                (string-append (first ele) "=" (first (tail ele)))
-                ele))
+          (map (l (ele) (if (list? ele) (string-append (first ele) "=" (first (tail ele))) ele))
             enum-list)
           ","))))
 
@@ -111,27 +103,21 @@
 
   (define (c-identifier arg)
     (string-append
-      (cond
-        ((symbol? arg) (symbol->string arg))
-        ((string? arg) arg)
+      (cond ((symbol? arg) (symbol->string arg)) ((string? arg) arg)
         (else (raise (q cannot-convert-to-c-identifier))))))
 
   (define (c-identifier-list arg)
     (parenthesise
       (if (list? arg) (string-join (map c-identifier arg) ",")
-        (if (or (symbol? arg) (string? arg))
-          (c-identifier arg)
+        (if (or (symbol? arg) (string? arg)) (c-identifier arg)
           (raise (q cannot-convert-to-c-identifier))))))
 
-  (define (c-identifier+type arg type)
-    (string-append (c-identifier type) " " (c-identifier arg)))
+  (define (c-identifier+type arg type) (string-append (c-identifier type) " " (c-identifier arg)))
 
   (define (c-parameter arg types)
     (parenthesise
-      (if (list? arg)
-        (string-join (map c-identifier+type arg types) ",")
-        (if (or (symbol? arg) (string? arg))
-          (c-identifier+type arg types)
+      (if (list? arg) (string-join (map c-identifier+type arg types) ",")
+        (if (or (symbol? arg) (string? arg)) (c-identifier+type arg types)
           (raise (q cannot-convert-to-c-parameter))))))
 
   (define (c-function-nc name type body parameter)
@@ -140,69 +126,47 @@
   (define* (c-function name type body #:optional (parameter (list)) (types (list)))
     (c-function-nc (c-identifier name) (c-identifier type) body (c-parameter parameter types)))
 
-  (define* (c-apply-nc proc-name #:optional (args ""))
-    (string-append proc-name "(" args ")"))
+  (define* (c-apply-nc proc-name #:optional (args "")) (string-append proc-name "(" args ")"))
 
   (define* (c-apply proc-name #:optional (args (list)))
     (c-apply-nc (c-identifier proc-name) (string-join (map c-value args) ",")))
 
-  (define* (c-if test consequent #:optional alternate) "string string [string] -> string
+  (define* (c-if test consequent #:optional alternate)
+    "string string [string] -> string
     create an if expression"
     (string-append "(" test "?" consequent ":" (if alternate alternate "0") ")"))
 
-  (define* (c-if-statement test consequent #:optional alternate) "string string [string] -> string
+  (define* (c-if-statement test consequent #:optional alternate)
+    "string string [string] -> string
     create an if statement"
-    (string-append
-      "if(" test "){" consequent "}"
-      (if alternate
-        (string-append "else{" alternate "}")
-        "")))
+    (string-append "if(" test
+      "){" consequent "}" (if alternate (string-append "else{" alternate "}") "")))
 
   (define (c-pointer-deref-nc arg key)
-    (if key
-      (string-append "*(" arg "+" key ")")
-      (string-append "(*" arg ")")))
+    (if key (string-append "*(" arg "+" key ")") (string-append "(*" arg ")")))
 
-  (define (c-bit-shift-nc left? arg shift-by)
-    (string-append arg (if left? "<<" ">>") shift-by))
-
+  (define (c-bit-shift-nc left? arg shift-by) (string-append arg (if left? "<<" ">>") shift-by))
   (define (c-set-nc name value) (string-append name "=" value))
   (define (c-set name value) (c-set-nc (c-identifier name) (c-value value)))
-  (define (c-pointer type) (string-append type " * " ))
+  (define (c-pointer type) (string-append type " * "))
   (define (c-address-of arg) (string-append "&" arg))
 
   (define (c-function-pointer-nc name return-type . types)
     (string-append return-type "(*" name ")(" (string-join types ",") ")"))
 
   (define (c-struct-ref-nc arg key) (string-append arg "." key))
-  (define (c-struct-ref arg key)
-    (c-struct-ref-nc (c-identifier arg) (c-identifier key)))
+  (define (c-struct-ref arg key) (c-struct-ref-nc (c-identifier arg) (c-identifier key)))
 
   (define c-escape-single-char
-    (alist
-      "\"" "\\\\"
-      "\a" "\\a"
-      "\n" "\\n"
-      "\b" "\\b"
-      "\f" "\\f"
-      "\r" "\\r"
-      "\t" "\\t"
-      "\v" "\\v"))
+    (alist "\"" "\\\\" "\a" "\\a" "\n" "\\n" "\b" "\\b" "\f" "\\f" "\r" "\\r" "\t" "\\t" "\v" "\\v"))
 
   (define (c-string str)
     (string-enclose
-      (fold
-        (l (ele res)
-          (string-replace-string res (first ele) (tail ele)))
-        str c-escape-single-char)
+      (fold (l (ele res) (string-replace-string res (first ele) (tail ele))) str
+        c-escape-single-char)
       "\""))
 
-  (define (c-value arg)
-    "handles the default conversions between scheme and c types"
-    (cond
-      ((symbol? arg) (symbol->string arg))
-      ((string? arg) (c-string arg))
-      ((number? arg) (number->string arg))
-      ((boolean? arg) (if arg "1" "0"))
-      ((char? arg) (string-enclose (any->string arg) "'"))
-      (else (raise (q cannot-convert-to-c))))))
+  (define (c-value arg) "handles the default conversions between scheme and c types"
+    (cond ((symbol? arg) (symbol->string arg)) ((string? arg) (c-string arg))
+      ((number? arg) (number->string arg)) ((boolean? arg) (if arg "1" "0"))
+      ((char? arg) (string-enclose (any->string arg) "'")) (else (raise (q cannot-convert-to-c))))))
