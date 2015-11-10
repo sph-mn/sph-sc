bdd4903d8f76fbb21e386d22370479224fa52c45
sph <sph@posteo.eu>
2015-11-10 08:23:42 +0000

    -

--- sph/lang/sc.scm
+++ sph/lang/sc.scm
@@ -1,54 +1,43 @@
 ;a scheme-datum to c compiler
+
 (library (sph lang sc)
-  (export sc->c sc-default-load-paths)
+  (export
+    sc->c
+    sc-default-load-paths)
   (import
-    (rnrs base)
-    (except (srfi srfi-1) map)
     (guile)
     (ice-9 match)
+    (rnrs base)
     (sph)
     (sph conditional)
-    (only (sph string) string-replace-string string-case parenthesise)
     (sph lang c expressions)
     (sph lang sc expressions)
+    (except (srfi srfi-1) map)
     (only (sph filesystem) search-load-path ensure-trailing-slash)
-    (only (sph read-write) file->datums)
     (only (sph list) map-slice length-eq-one?)
+    (only (sph read-write) file->datums)
+    (only (sph string)
+      string-replace-string
+      string-case
+      parenthesise)
     (only (sph tree) tree-transform tree-contains?))
 
   (define sc-default-load-paths
     (map ensure-trailing-slash
-      (pass-if (getenv "SC_LOAD_PATH")
-        (l (a) (string-split a #\:)) (list))))
+      (pass-if (getenv "SC_LOAD_PATH") (l (a) (string-split a #\:)) (list))))
 
   (define-as identical-infix-token list "+" "-" "<" ">" "<=" ">=" "*" "/")
 
   (define-as translated-infix-token list
-    "equal_p"
-    "or"
-    "and"
-    "="
-    "bit_or"
-    "bit_and"
-    "bit_xor"
-    "modulo"
-    "bit_shift_right"
-    "bit_shift_left"
-    "eqv_p"
-    "eq_p")
+    "equal_p" "or"
+    "and" "=" "bit_or" "bit_and" "bit_xor" "modulo" "bit_shift_right" "bit_shift_left" "eqv_p" "eq_p")
 
   (define (translate-infix-token a)
-    (string-case a
-      (("=" "equal_p" "eqv_p" "eq_p") "==")
-      ("and" "&&")
-      ("bit_or" "|")
-      ("bit_and" "&")
-      ("or" "||")
-      ("modulo" "%")
-      ("bit_shift_right" ">>")
-      ("bit_shift_left" "<<")
-      ("bit_xor" "^")
-      (raise (q cannot-convert-symbol-to-c))))
+    (string-case a (("=" "equal_p" "eqv_p" "eq_p") "==")
+      ("and" "&&") ("bit_or" "|")
+      ("bit_and" "&") ("or" "||")
+      ("modulo" "%") ("bit_shift_right" ">>")
+      ("bit_shift_left" "<<") ("bit_xor" "^") (raise (q cannot-convert-symbol-to-c))))
 
   (define (join-expressions a)
     (string-join
@@ -58,10 +47,8 @@
             ;preprocessor directives need to start on a separate line
             (if (string-prefix? "#" e)
               (if (or (null? prev) (not (string-prefix? "\n" (first prev))))
-                (string-append "\n" e "\n")
-                (string-append "\n" e))
-              (if (or (string-suffix? "}" e) (string-suffix? ";" e))
-                e
+                (string-append "\n" e "\n") (string-append "\n" e))
+              (if (or (string-suffix? "}" e) (string-suffix? ";" e)) e
                 (if (string-suffix? ":" e) (string-append e "\n") (string-append e ";"))))
             prev))
         (list) a)
@@ -75,28 +62,23 @@
       (map
         (l (e)
           (match e
-            ((name type ... (? integer? bits))
-              (string-append
-                (string-join (map sc-identifier type) " " (q suffix))
+            ( (name type ... (? integer? bits))
+              (string-append (string-join (map sc-identifier type) " " (q suffix))
                 (sc-identifier name) ":" (sc-value bits)))
             (((quote function-pointer) name type ...) (compile e))
-            ((name type ...)
-              (string-append
-                (string-join (map sc-identifier type) " " (q suffix))
+            ( (name type ...)
+              (string-append (string-join (map sc-identifier type) " " (q suffix))
                 (sc-identifier name)))))
         elements)
       ";" (q suffix)))
 
   (define (ascend-expr->c a)
     ;these are applied when ascending the tree
-    (string-case (first a)
-      ("not" (string-append "!" (apply string-append (tail a))))
+    (string-case (first a) ("not" (string-append "!" (apply string-append (tail a))))
       ("deref"
-        (let (a (tail a))
-          (c-pointer-deref-nc (first a) (if (null? (tail a)) #f (first (tail a))))))
+        (let (a (tail a)) (c-pointer-deref-nc (first a) (if (null? (tail a)) #f (first (tail a))))))
       ("address_of" (apply c-address-of (tail a)))
-      ("convert_type" (apply c-convert-type-nc (tail a)))
-      ("begin" (join-expressions (tail a)))
+      ("convert_type" (apply c-convert-type-nc (tail a))) ("begin" (join-expressions (tail a)))
       ("struct_ref" (apply c-struct-ref-nc (tail a)))
       ("return" (if (null? (tail a)) "return" (sc-apply (first a) (tail a))))
       ("goto" (string-join a " "))
@@ -117,31 +99,22 @@
   (define (descend-expr->sc a compile load-paths)
     ;these are applied when descending the tree, and the result is parsed again
     (case (first a)
-      ((struct-set)
+      ( (struct-set)
         (let (struct-var (first (tail a)))
           (pair (q begin)
             (map-slice 2
-              (l (field value)
-                (list (q set) (list (q struct-ref) struct-var field) value))
+              (l (field value) (list (q set) (list (q struct-ref) struct-var field) value))
               (tail (tail a))))))
-      ((include-sc)
+      ( (include-sc)
         (pair (q begin)
-          (file->datums
-            (search-load-path
-              (string-append (first (tail a)) ".sc")
-              load-paths)
-            read)))
-      ((cond cond*)
-        (let
-          ( (cond (reverse (tail a)))
-            (symbol-if (if (eqv? (first a) (q cond*)) (q if*) (q if))))
+          (file->datums (search-load-path (string-append (first (tail a)) ".sc") load-paths) read)))
+      ( (cond cond*)
+        (let ((cond (reverse (tail a))) (symbol-if (if (eqv? (first a) (q cond*)) (q if*) (q if))))
           (fold
             (l (cond alternate)
               (list symbol-if (first cond) (add-begin-if-multiple (tail cond)) alternate))
-            (match (first cond)
-              (((quote else) body ...) (add-begin-if-multiple body))
-              ((test consequent ...)
-                (list symbol-if test (add-begin-if-multiple consequent))))
+            (match (first cond) (((quote else) body ...) (add-begin-if-multiple body))
+              ((test consequent ...) (list symbol-if test (add-begin-if-multiple consequent))))
             (tail cond))))
       (else #f)))
 
@@ -149,84 +122,67 @@
     ;these are applied when descending the tree, and the result is not parsed again.
     ;this is for expressions that create syntax that can not be created with basic sc syntax
     (case (first a)
-      ((set)
+      ( (set)
         (match (tail a)
-          ((name-1 value-1 name-2 value-2 rest ...)
+          ( (name-1 value-1 name-2 value-2 rest ...)
             (join-expressions (map-slice 2 c-set-nc (map compile (tail a)))))
-          ((name value)
-            (c-set-nc
-              (compile name)
-              (compile value)))))
-      ((define)
+          ((name value) (c-set-nc (compile name) (compile value)))))
+      ( (define)
         (match (tail a)
-          (((name parameter ...) (return-type types ...) body ...)
-            (c-function-nc
-              (sc-identifier (compile name))
-              (sc-identifier return-type)
+          ( ( (name parameter ...) (return-type types ...) body ...)
+            (c-function-nc (sc-identifier (compile name)) (sc-identifier return-type)
               (if (null? body) #f (join-expressions (map compile body)))
-              (c-parameter
-                (map sc-identifier parameter)
-                (map sc-identifier types))))
-          (((name parameter ...) return-type body ...)
-            (c-function
-              (sc-identifier (compile name))
-              (sc-identifier return-type)
+              (c-parameter (map sc-identifier parameter) (map sc-identifier types))))
+          ( ( (name parameter ...) return-type body ...)
+            (c-function (sc-identifier (compile name)) (sc-identifier return-type)
               (if (null? body) #f (join-expressions (map compile body)))))
-          ((name-1 type-1 name-2 type-2 rest ...)
+          ( (name-1 type-1 name-2 type-2 rest ...)
             (join-expressions (map-slice 2 sc-define (tail a))))
-          ((name type value ...)
-            (c-define-nc
-              (sc-identifier name)
-              (sc-identifier type)
+          ( (name type value ...)
+            (c-define-nc (sc-identifier name) (sc-identifier type)
               (if (null? value) #f (compile (first value)))))))
-      ((define-array)
+      ( (define-array)
         (match (tail a)
-          ((name type size values ...)
-            (c-define-array-nc
-              (sc-identifier name)
-              (sc-identifier type)
-              (compile size)
-              (if (null? values) #f (map compile values))))))
-      ((if)
-        (apply c-if-statement
-          (compile (first (tail a)))
+          ( (name type size values ...)
+            (c-define-array-nc (sc-identifier name) (sc-identifier type)
+              (compile size) (if (null? values) #f (map compile values))))))
+      ( (if)
+        (apply c-if-statement (compile (first (tail a)))
           (map (l (e) (compile (list (q begin) e))) (tail (tail a)))))
-      ((if-expression if*)
+      ( (if-expression if*)
         (apply c-if
           (map
             (l (e)
               (match e
-                (((quote begin) body ...)
+                ( ( (quote begin) body ...)
                   (parenthesise
                     (string-join
                       (map
                         (l (e)
-                          (if (and (list? e) (tree-contains? e (q set)))
-                            (parenthesise (compile e))
+                          (if (and (list? e) (tree-contains? e (q set))) (parenthesise (compile e))
                             (compile e)))
                         body)
                       ",")))
                 (_
-                  (if (and (list? e) (tree-contains? e (q set)))
-                    (parenthesise (compile e))
+                  (if (and (list? e) (tree-contains? e (q set))) (parenthesise (compile e))
                     (compile e)))))
             (tail a))))
-      ((define-macro)
+      ( (define-macro)
         (match (tail a)
-          (((name parameter ...) body ...)
+          ( ( (name parameter ...) body ...)
             (cp-define-macro-nc (sc-identifier name)
               (string-trim-right (join-expressions (map compile body)) #\;)
               (sc-identifier-list parameter)))
-          ((name-1 value-1 name-2 value-2 rest ...)
+          ( (name-1 value-1 name-2 value-2 rest ...)
             (string-join
-              (map-slice
-                2 (l (name value) (cp-define-macro-nc (sc-identifier name) (compile value) #f))
+              (map-slice 2
+                (l (name value) (cp-define-macro-nc (sc-identifier name) (compile value) #f))
                 (tail a))
               "\n"))
-          ((name body ...)
-
-            (cp-define-macro-nc (sc-identifier name) (string-trim-right (join-expressions (map compile body)) #\;) #f))))
-      ((struct union)
+          ( (name body ...)
+            (cp-define-macro-nc (sc-identifier name)
+              (string-trim-right (join-expressions (map compile body)) #\;) #f))))
+      ( (struct union)
         (let (tail-a (tail a))
           (apply
             (l (name body)
@@ -235,79 +191,61 @@
             (if (symbol? (first tail-a))
               (list (string-append " " (sc-identifier (first tail-a))) (tail tail-a))
               (list "" tail-a)))))
-      ((struct-init)
-        (string-append (c-compound-nc (string-join (map compile (tail a)) ",")) ";"))
-      ((while)
+      ((struct-init) (string-append (c-compound-nc (string-join (map compile (tail a)) ",")) ";"))
+      ( (while)
         (match (tail a)
-          ((test body ...)
-            (string-append "while"
-              (parenthesise (compile test))
-              (c-compound-nc
-                (compile (pair (q begin) body)))))))
-      ((do-while)
+          ( (test body ...)
+            (string-append "while" (parenthesise (compile test))
+              (c-compound-nc (compile (pair (q begin) body)))))))
+      ( (do-while)
         (match (tail a)
-          ((test body ...)
-            (string-append "do"
-              (c-compound-nc
-                (compile (pair (q begin) body)))
+          ( (test body ...)
+            (string-append "do" (c-compound-nc (compile (pair (q begin) body)))
               "while" (parenthesise (compile test))))))
-      ((undefine-macro)
+      ( (undefine-macro)
         (string-join (map (compose cp-undef sc-identifier) (tail a)) "\n" (q suffix)))
-      ((let-macro)
+      ( (let-macro)
         ;descend-expr->sc currently would add an uneccessary semicolon at the end
         (match (tail a)
-          (((names+values ...) body ...)
+          ( ( (names+values ...) body ...)
             (string-append
               (string-join
-                (map-slice 2
-                  (l (n v) (compile (list (q define-macro) n v)))
-                  names+values)
-                "\n" (q suffix))
+                (map-slice 2 (l (n v) (compile (list (q define-macro) n v))) names+values) "\n"
+                (q suffix))
               (compile (pair (q begin) body))
               (string-join
                 (map-slice 2
-                  (l (n v)
-                    (compile (list (q undefine-macro) (if (pair? n) (first n) n))))
+                  (l (n v) (compile (list (q undefine-macro) (if (pair? n) (first n) n))))
                   names+values)
                 "\n" (q prefix))))
           (_ (raise (q syntax-error-for-let-macro)))))
       ((include) (apply string-append (map cp-include (tail a))))
       ((includep) (apply string-append (map cp-includep (tail a))))
-      ((function-pointer)
+      ( (function-pointer)
         (let (tail-a (tail a))
           (apply c-function-pointer-nc (sc-identifier (first tail-a))
-            (map
-              (l (e)
-                (if (list? e) (string-join (map sc-identifier e) " ")
-                  (sc-identifier e)))
+            (map (l (e) (if (list? e) (string-join (map sc-identifier e) " ") (sc-identifier e)))
               (tail tail-a)))))
       ((pre-concat) (cp-concat-nc (map sc-identifier (tail a))))
-      ((pre-if) (scp-if (q if) (tail a) compile))
-      ((pre-ifdef) (scp-if (q ifdef) (tail a) compile))
-      ((pre-ifndef) (scp-if (q ifndef) (tail a) compile))
-      ((quote) (list-ref a 1))
-      ((let*)
+      ((pre-if) (scp-if (q if) (tail a) compile)) ((pre-ifdef) (scp-if (q ifdef) (tail a) compile))
+      ((pre-ifndef) (scp-if (q ifndef) (tail a) compile)) ((quote) (list-ref a 1))
+      ( (let*)
         (c-compound-nc
           (match (tail a)
-            ( (((names values ...) ...) body ...)
+            ( ( ( (names values ...) ...) body ...)
               (compile
                 (pair (q begin)
                   (append
-                    (map
-                      (l (n v)
-                        (pairs
-                          (if (length-eq-one? v) (q set) (q define))
-                          n v))
-                      names values)
+                    (map (l (n v) (pairs (if (length-eq-one? v) (q set) (q define)) n v)) names
+                      values)
                     body)))))))
       (else #f)))
 
   (define (descend-proc load-paths)
     (l (a compile)
       (let (r (descend-expr->sc a compile load-paths))
-        (if r (list r #t)
-          (let (r (descend-expr->c a compile))
-            (if r (list r #f) (list #f #t)))))))
+        (if r (list r #t) (let (r (descend-expr->c a compile)) (if r (list r #f) (list #f #t)))))))
 
-  (define* (sc->c a #:optional (load-paths sc-default-load-paths)) ;expression -> string
+  (define* (sc->c a #:optional (load-paths sc-default-load-paths))
+    ;expression -> string
     (tree-transform a (descend-proc load-paths) ascend-expr->c sc-value)))
