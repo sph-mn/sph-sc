af7f89a577df02e5d0eeee5affa94e631880591c
sph-mn <sph@posteo.eu>
2016-05-18 20:18:43 +0000

    positive integers are not explicitly unsigned by default anymore. renaming of include and includep to only pre-include. add pre-include-once. add enum syntax. fix array and function pointer syntax by allowing false as an argument for size and name respectively to not have it specified

--- modules/sph/lang/sc/expressions.scm
+++ modules/sph/lang/sc/expressions.scm
@@ -1,9 +1,12 @@
 (library (sph lang sc expressions)
   (export
     sc-apply
+    sc-case
     sc-define
     sc-identifier
     sc-identifier-list
+    sc-pre-include
+    sc-pre-include-once
     sc-value
     scp-if
     translate-identifier)
@@ -13,8 +16,12 @@
     (rnrs base)
     (sph)
     (sph lang c expressions)
-    (only (guile) make-regexp string-join)
+    (only (guile)
+      string-prefix?
+      make-regexp
+      string-join)
     (only (sph alist) alist)
+    (only (sph list) map-slice)
     (only (sph one) alist->regexp-match-replacements)
     (only (sph string) regexp-match-replace))
 
@@ -42,14 +49,47 @@
   (define (sc-identifier-list a) (string-append "(" (string-join (map sc-identifier a) ",") ")"))
 
   (define (sc-value a)
-    (cond ((symbol? a) (translate-identifier (symbol->string a)))
-      ((and (integer? a) (<= 0 a)) (string-append (number->string a) "u"))
-      ((boolean? a) (if a "1u" "0u")) (else (c-value a))))
+    (cond ((symbol? a) (translate-identifier (symbol->string a))) ((boolean? a) (if a "1" "0"))
+      (else (c-value a))))
 
   (define (scp-if type a compile)
     (match a
       ( (test consequent alternate)
         (cp-if type (compile test) (compile (add-begin consequent)) (compile (add-begin alternate))))
       ((test consequent) (cp-if type (compile test) (compile (add-begin consequent)) #f))))
+
+  (define (sc-case cond-name predicate subject . clauses) "cond-name is either cond or cond*"
+    (pair cond-name
+      (map
+        (l (a)
+          (match a (((quote else) _ ...) a)
+            ( ( (objects ...) body ...)
+              (pair (pair (q or) (map (l (b) (list predicate b subject)) objects)) body))
+            ((object body ...) (pair (list predicate object subject) body))))
+        clauses)))
+
+  (define (sc-pre-include paths)
+    "(string ...) -> string
+    uses c load path <> notation when a given path does not start with a slash or with a directory reference"
+    (string-join
+      (map
+        (l (a)
+          ( (if (or (string-prefix? "./" a) (string-prefix? "../" a) (string-prefix? "/" a))
+              cp-include-path cp-include)
+            a))
+        paths)
+      "\n" (q suffix)))
+
+  (define (sc-pre-include-once names/paths)
+    "([symbol string] ...)
+    unfortunately it seems too difficult to guess globally identifying names for included files, therefore they have to be specified"
+    (string-join
+      (map-slice 2
+        (l (name path)
+          (let* ((name (sc-identifier name)) (variable-name (string-append "sc_included_" name)))
+            (cp-if (q ifndef) variable-name
+              (string-append (sc-pre-include (list path)) (cp-define-macro-nc variable-name "" "")))))
+        names/paths)
+      "\n"))
 
   (define (translate-identifier a) (regexp-match-replace a identifier-replacements)))
