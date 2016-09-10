d5a8aa771b14f84d2445e9311f5546a72c888daf
sph <tantalum@online.de>
2016-09-10 21:05:13 +0000

    make "struct-literal" support simple struct initialisers (like arrays) and designated initialisers with ((key value) ...)

--- modules/test/module/sph/lang/sc.scm
+++ modules/test/module/sph/lang/sc.scm
@@ -97,4 +97,4 @@
       (enum test (a b c d e)) "enum test{a,b,c,d,e};"
       (enum (a b c d e)) "enum{a,b,c,d,e};"
       (enum (a b (c 3) d (e 4))) "enum{a,b,c=3,d,e=4};"
-      (array-literal 1 "2" 3 4) "{1,\"2\",3,4};" (struct-literal a 1 b "2") "{.a=1,.b=\"2\"};")))
+      (array-literal 1 "2" 3 4) "{1,\"2\",3,4};" (struct-literal (a 1) (b "2")) "{.a=1,.b=\"2\"};" (struct-literal a 1) "{a,1};")))
