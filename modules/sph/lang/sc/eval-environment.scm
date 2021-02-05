f0827d092c94ebe86f811affb0aca757fbe449f0
sph <sph@posteo.eu>
2021-02-05 20:13:28 +0100

    rename sc-count-expressions to sc-count-expression. add sc-contains-expression

--- modules/sph/lang/sc/eval-environment.scm
+++ modules/sph/lang/sc/eval-environment.scm
@@ -1,18 +1,20 @@
 (define-module (sph lang sc eval-environment))
-(use-modules (srfi srfi-1) (sph list) (sph alist) (sph hashtable) (sph) (sph lang sc))
+
+(use-modules (srfi srfi-1) (sph list)
+  (sph alist) (sph hashtable) (sph) (sph lang sc) ((sph tree) #:select (tree-any)))
 
 (re-export any->list flatten
   contains? containsq?
   containsv? first
   map-with-index tail pair null pairs q qq debug-log sc-gensym sc-syntax? alist-ref map-integers l)
 
-(export sc-count-expressions sph-lang-sc-eval-environment-description)
+(export sc-contains-expression sc-count-expressions sph-lang-sc-eval-environment-description)
 
 (define sph-lang-sc-eval-environment-description
   "additional helpers available in sc-define-syntax*")
 
-(define (sc-count-expressions prefix a)
-  "count all sub-expressions in a that begin with prefix, ignore content in sc-comment"
+(define (sc-count-expression prefix a)
+  "count all sub-expressions in $a that begin with prefix, ignore content in sc-comment"
   (let loop ((a a) (result 0))
     (if (list? a)
       (fold
@@ -21,3 +23,11 @@
             (loop (tail a) (if (eq? prefix (first a)) (+ result 1) result)) result))
         result a)
       result)))
+
+(define (sc-contains-expression prefix a)
+  "return true if any sub-expression in $a begins with prefix, ignore content in sc-comment.
+   use case: conditionally add code if certain expressions were used"
+  (tree-any
+    (l (a)
+      (and (list? a) (not (null? a)) (not (eq? (q sc-comment) (first a))) (eq? prefix (first a))))
+    a))
