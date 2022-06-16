933db7d179f14609f11797d8d0e3d07beea49f53
sph <sph@posteo.eu>
2022-06-16 16:20:59 +0200

    add for-each-index as a default macro. sc-format: make custom expression distribution configuration generic

--- modules/sph/lang/sc/syntax-extensions.scm
+++ modules/sph/lang/sc/syntax-extensions.scm
@@ -3,7 +3,8 @@
 
 (define sc-syntax-extensions
   (quasiquote
-    ( (sc-define-syntax (1+ x) (+ x 1))
-      (sc-define-syntax (1- x) (- x 1))
+    ( (sc-define-syntax (1+ x) (+ x 1)) (sc-define-syntax (1- x) (- x 1))
       (sc-define-syntax (when test-expr body ...) (if test-expr (begin body ...)))
-      (sc-define-syntax (unless test-expr body ...) (when (not test-expr) body ...)))))
+      (sc-define-syntax (unless test-expr body ...) (when (not test-expr) body ...))
+      (sc-define-syntax (for-each-index index type limit body ...)
+        (for ((define index type 0) (< index limit) (set+ index 1)) body ...)))))
