c5e664134baa52985cde4baa02c5a9730b42c324
sph <sph@posteo.eu>
2025-05-30 00:11:40 +0200

    add the macro for-each-index-from

--- modules/sph/lang/sc/syntax-extensions.scm
+++ modules/sph/lang/sc/syntax-extensions.scm
@@ -6,5 +6,7 @@
     ( (sc-define-syntax (1+ x) (+ x 1)) (sc-define-syntax (1- x) (- x 1))
       (sc-define-syntax (when test-expr body ...) (if test-expr (begin body ...)))
       (sc-define-syntax (unless test-expr body ...) (when (not test-expr) body ...))
+      (sc-define-syntax (for-each-index-from from index type limit body ...)
+        (for ((define index type from) (< index limit) (set+ index 1)) body ...))
       (sc-define-syntax (for-each-index index type limit body ...)
-        (for ((define index type 0) (< index limit) (set+ index 1)) body ...)))))
+        (for-each-index-from 0 index type limit body ...)))))
