b21338f925366c46d5d7b6b941918e26fd222ba9
sph <sph@posteo.eu>
2025-10-03 13:34:10 +0200

    fix sph-sc-mode preventing the initializion of other modes when it loads

--- other/emacs/sph-sc-mode.el
+++ other/emacs/sph-sc-mode.el
@@ -1,5 +1,5 @@
 ;;; sph-sc-mode.el -*- lexical-binding: t; -*-
-; version 2025-08-09
+; version 2025-10-03
 
 (defgroup sph-sc nil "Scheme-like s-expr editing" :group 'languages)
 (defcustom sph-sc-compress-on-indent t "Compress spaces on indent/newline outside strings/comments." :type 'boolean :group 'sph-sc)
@@ -7,7 +7,7 @@
 (defface sph-sc-bracket-face '((t :inherit default)) "Face for ()" :group 'sph-sc)
 (defalias 'sequence-ref 'elt)
 
-(defvar scheme-mode-syntax-table
+(defvar sph-sc-mode-syntax-table
   (let ((st (make-syntax-table)) (i 0))
     (while (< i ?0) (modify-syntax-entry i "_ " st) (setq i (1+ i)))
     (setq i (1+ ?9))
@@ -34,13 +34,13 @@
     st))
 
 (defconst sph-sc-sexp-comment-syntax-table
-  (let ((st (make-syntax-table scheme-mode-syntax-table)))
+  (let ((st (make-syntax-table sph-sc-mode-syntax-table)))
     (modify-syntax-entry ?\; "." st)
     (modify-syntax-entry ?\n " " st)
     (modify-syntax-entry ?# "'" st)
     st))
 
-(defun lisp-font-lock-syntactic-face-function (state)
+(defun sph-sc--lfl-sff (state)
   (if (nth 3 state)
     (let ((startpos (nth 8 state)))
       (if (eq (char-after startpos) ?|)
@@ -88,13 +88,13 @@
         (when (< pos (- end 2))
           (put-text-property pos (- end 2) 'syntax-table sph-sc-sexp-comment-syntax-table))
         (put-text-property (- end 1) end 'syntax-table '(12)))))
-  (lisp-font-lock-syntactic-face-function state))
+  (sph-sc--lfl-sff state))
 
 (defconst sph-sc-font-lock-keywords-1
   (list
     '("(\\(define[^ ]*?\\) +(\\(\\sw+\\)"
       (1 font-lock-keyword-face) (2 font-lock-function-name-face))
-    '("[()]" . sph-sc-bracket-face)))
+    '("[()]" 0 'sph-sc-bracket-face prepend)))
 
 (defconst sph-sc-font-lock-keywords-2
   `(
@@ -102,7 +102,7 @@
     ("(\\(pre-\\w+\\)" (1 font-lock-preprocessor-face))
     ("(\\(sc-\\w+\\)" (1 font-lock-preprocessor-face))
     ("(\\(define[^ ]*?\\) +(\\(\\sw+\\) \\(.*?\\))" (2 font-lock-function-name-face))
-    ("[()]" . sph-sc-bracket-face)
+    ("[()]" 0 'sph-sc-bracket-face prepend)
     ("(\\(define[^? ]*?\\) +\\(\\sw+\\)" (2 font-lock-variable-name-face))
     ("(\\(label\\) +\\(\\sw+\\)" (2 font-lock-function-name-face))
     (,(concat "(\\(" (regexp-opt '("debug-log" "set" "declare") t) "\\)\\>") 1 'font-lock-builtin-face)
@@ -142,10 +142,10 @@
 
 ;;;###autoload
 (define-derived-mode sph-sc-mode prog-mode "sph-sc"
-  :syntax-table scheme-mode-syntax-table
+  :syntax-table sph-sc-mode-syntax-table
   :keymap sph-sc-mode-map
   (setq-local parse-sexp-ignore-comments t)
-  (setq-local indent-line-function 'sph-sc-indent-line)
+  (setq-local indent-line-function #'sph-sc-indent-line)
   (setq-local comment-start ";")
   (setq-local comment-start-skip "\\(\\(^\\|[^\\\\\n]\\)\\(\\\\\\\\\\)*\\);+[ \t]*")
   (setq-local comment-add 1)
@@ -160,6 +160,12 @@
       (font-lock-syntactic-face-function . sph-sc-font-lock-syntactic-face-function)
       (parse-sexp-lookup-properties . t)
       (font-lock-extra-managed-props syntax-table)))
+  (when (not sph-sc-highlight-brackets)
+    (setq-local sph-sc-font-lock-keywords-1
+      (remq '("[()]" 0 'sph-sc-bracket-face prepend) sph-sc-font-lock-keywords-1))
+    (setq-local sph-sc-font-lock-keywords-2
+      (remq '("[()]" 0 'sph-sc-bracket-face prepend) sph-sc-font-lock-keywords-2))
+    (setq-local sph-sc-font-lock-keywords sph-sc-font-lock-keywords-1))
   (font-lock-refresh-defaults)
   (when (fboundp 'font-lock-flush) (font-lock-flush))
   (when (fboundp 'font-lock-ensure) (font-lock-ensure)))
